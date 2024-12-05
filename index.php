5da44761b762964657ab4ae98f32b714430289fa
ahmadh-najahi <ahmednajahi2003@gmail.com>
2024-12-05 07:08:44 +0530

    This is a Simple Login Page

---
+++ index.php
@@ -0,0 +1,33 @@
+<php>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="login-container">
+        <form class="login-form">
+            <h2>Login</h2>
+            <div class="input-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="remember-me">
+                <input type="checkbox" id="remember" name="remember">
+                <label for="remember">Remember me</label>
+            </div>
+            <button type="submit">Login</button>
+            <p class="signup">Don't have an account? <a href="#">Signup</a></p>
+        </form>
+    </div>
+</body>
+</html>
+
+</php>
